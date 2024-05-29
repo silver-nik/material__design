0f10927c6722f335487f13102a7063d389afce4a
Shvetsov Nikita <send0@yandex.ru>
2024-05-29 16:57:33 +1000

    create mobile menu

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
     const toggleTheme = () => {
         let settingTheme = document.querySelector('[data-bs-theme]');
 
-        switch(     settingTheme.getAttribute('data-bs-theme')) {
+        switch(settingTheme.getAttribute('data-bs-theme')) {
             case 'dark':
                 document.querySelector('[data-bs-theme]').setAttribute('data-bs-theme', 'light');
                 break;
@@ -12,5 +12,26 @@
             default:
 
         }
-    } 
-        
+    }
+
+    const toggleBurgerMenu = (e) => {
+        document.querySelectorAll('[data-toggle]').forEach(el => {
+            console.log(el);
+
+            if (el.getAttribute('data-toggle') === 'true') {
+                el.setAttribute('data-toggle', false)
+                el.classList.add("d-none");
+                el.classList.remove("d-flex");
+
+                e.target.closest('nav').classList.remove('vh-100');
+
+            } else if (el.getAttribute('data-toggle') === 'false') {
+                el.setAttribute('data-toggle', true)
+                el.classList.remove("d-none");
+                el.classList.add("d-flex");
+
+                e.target.closest('nav').classList.add('vh-100');
+
+            }
+        });
+    }
